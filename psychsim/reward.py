a0c957a8d428d77ff67db212a4e581035f1673e5
David V. Pynadath <david@pynadath.net>
2024-06-18 00:54:46 +0800

    Added 'invert' option for achievement goal

--- psychsim/reward.py
+++ psychsim/reward.py
@@ -6,13 +6,27 @@ def maximizeFeature(key,agent):
 def minimizeFeature(key,agent):
     return KeyedTree(setToFeatureMatrix(rewardKey(agent),key,-1.))
 
+
 def achieveFeatureValue(key,value,agent):
+    return achieve_feature_value(key, value, agent)
+
+
+def achieve_feature_value(key,value,agent):
     return makeTree({'if': equalRow(key,value),
                      True: setToConstantMatrix(rewardKey(agent),1.),
                      False: setToConstantMatrix(rewardKey(agent),0.)})
 
-def achieveGoal(key,agent):
-    return makeTree({'if': trueRow(key),
+
+def achieveGoal(key, agent, invert=False):
+    return achieve_goal(key, agent, invert)
+
+
+def achieve_goal(key, agent, invert=False):
+    """
+    :param invert: if C{True}, then reward is gained if the feature is False, 
+                   not True (as is the default)
+    """
+    return makeTree({'if': falseRow(key) if invert else trueRow(key),
                      True: setToConstantMatrix(rewardKey(agent),1.),
                      False: setToConstantMatrix(rewardKey(agent),0.),})
 
